e7013736d49b07b65ea847c8106031cab416e1ae
dababadal <43194076+dababadal@users.noreply.github.com>
2019-12-12 19:46:43 +0100

    e

---
+++ script.js
@@ -0,0 +1,9 @@
+function board(){
+
+    for (var i = 0; i < 8; i++) {
+        for (var j = 0; j < 8; j++) {
+            $(".chessboard").append('<div class="tile"></div>');
+        }
+        
+    }
+};
