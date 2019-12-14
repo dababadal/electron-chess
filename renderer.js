736f629b91404f008def6a8a36a32c8d803aa359
dababadal <43194076+dababadal@users.noreply.github.com>
2019-12-14 15:12:54 +0100

    modify

--- renderer.js
+++ renderer.js
@@ -1,28 +1,28 @@
-for(var x=1; x<9;x++) {
+for(var x=8; x>=1;x--) {
   for(var y=1; y<9;y++) {
   var file = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
   if(x%2==0){
     if(y%2==0){
     var board = document.createElement('div');
-    board.className = "white";
-    board.id = file[y] + x;
+    board.id = "black";
+    board.className = file[y] + x;
     document.getElementById('chessboard').appendChild(board);
     }else{
       var board = document.createElement('div');
-    board.className = "black";
-    board.id = file[y] + x;
+      board.id = "white";
+      board.className = file[y] + x;
     document.getElementById('chessboard').appendChild(board);
     }
   }else{
       if(y%2==0){
     var board = document.createElement('div');
-    board.className = "black";
-    board.id = file[y] + x;
+    board.id = "white";
+    board.className = file[y] + x;
     document.getElementById('chessboard').appendChild(board);
     }else{
       var board = document.createElement('div');
-    board.className = "white";
-    board.id = file[y] + x;
+      board.id = "black";
+      board.className = file[y] + x;
     document.getElementById('chessboard').appendChild(board);
     }
   }
