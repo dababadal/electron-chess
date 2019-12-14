14a1cae7a12db38dbd5825aa85f750018e6f0c4f
dababadal <43194076+dababadal@users.noreply.github.com>
2019-12-14 17:26:55 +0100

    finished board

--- renderer.js
+++ renderer.js
@@ -26,9 +26,20 @@ for(var x=8; x>=1;x--) {
     document.getElementById('chess_square').appendChild(board);
       }
     }
+    if(x==1){
+      if(y==1){}else{
     var nots = document.createElement('div');
     nots.id = "notation";
-    nots.innerHTML = " " + board.className;
-    document.getElementsByClassName(board.className)[0].appendChild(nots);
+    nots.innerHTML = file[y];
+    document.getElementsByClassName(board.className)[0].appendChild(nots);}
+    }
+    if(y==1){
+      if(x==1){}else{
+      var nots = document.createElement('div');
+      nots.id = "notation";
+      nots.innerHTML = x;
+      document.getElementsByClassName(board.className)[0].appendChild(nots);
+      }
+      }
   }
 }
