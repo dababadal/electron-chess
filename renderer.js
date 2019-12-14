570a0438d005bedc49945a41e77c8ddc4a89f3b1
dababadal <43194076+dababadal@users.noreply.github.com>
2019-12-14 14:46:04 +0100

    test

--- renderer.js
+++ renderer.js
@@ -1,27 +1,25 @@
-var board = document.createElement('div');
-            board.className = "blah";
-            for(var x=1; x<9;x++) {
-                for(var y=1; y<9;y++) {
-                if(x%2==0){
-                  if(y%2==0){
-                  var board = document.createElement('div');
-                  board.className = "white";
-                  document.getElementById('chessboard').appendChild(board);
-                  }else{
-                    var board = document.createElement('div');
-                  board.className = "black";
-                  document.getElementById('chessboard').appendChild(board);
-                  }
-                }else{
-                    if(y%2==0){
-                  var board = document.createElement('div');
-                  board.className = "black";
-                  document.getElementById('chessboard').appendChild(board);
-                  }else{
-                    var board = document.createElement('div');
-                  board.className = "white";
-                  document.getElementById('chessboard').appendChild(board);
-                  }
-                }
-                }
-              }
+for(var x=1; x<9;x++) {
+  for(var y=1; y<9;y++) {
+  if(x%2==0){
+    if(y%2==0){
+    var board = document.createElement('div');
+    board.className = "white";
+    document.getElementById('chessboard').appendChild(board);
+    }else{
+      var board = document.createElement('div');
+    board.className = "black";
+    document.getElementById('chessboard').appendChild(board);
+    }
+  }else{
+      if(y%2==0){
+    var board = document.createElement('div');
+    board.className = "black";
+    document.getElementById('chessboard').appendChild(board);
+    }else{
+      var board = document.createElement('div');
+    board.className = "white";
+    document.getElementById('chessboard').appendChild(board);
+    }
+  }
+  }
+}
