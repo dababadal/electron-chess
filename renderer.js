1c6debcedb4e3ce5bbdb36738c2ccf0c18156776
dababadal <43194076+dababadal@users.noreply.github.com>
2019-12-14 15:01:10 +0100

    add notation to tag

--- renderer.js
+++ renderer.js
@@ -1,23 +1,28 @@
 for(var x=1; x<9;x++) {
   for(var y=1; y<9;y++) {
+  var file = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
   if(x%2==0){
     if(y%2==0){
     var board = document.createElement('div');
     board.className = "white";
+    board.id = file[y] + x;
     document.getElementById('chessboard').appendChild(board);
     }else{
       var board = document.createElement('div');
     board.className = "black";
+    board.id = file[y] + x;
     document.getElementById('chessboard').appendChild(board);
     }
   }else{
       if(y%2==0){
     var board = document.createElement('div');
     board.className = "black";
+    board.id = file[y] + x;
     document.getElementById('chessboard').appendChild(board);
     }else{
       var board = document.createElement('div');
     board.className = "white";
+    board.id = file[y] + x;
     document.getElementById('chessboard').appendChild(board);
     }
   }
